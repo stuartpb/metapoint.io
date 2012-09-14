0bc11db8d9247c09da434b41f889e48b91ae767b
Stuart P. Bentley <stuart@testtrack4.com>
2012-09-14 14:23:58 -0700

    Mistaken approach to suggestions
    I started writing logic for a world where "suggestions" is a staging
    table that attempts to mirror the structure of "topics", and not one
    where each recieved suggestion is its own document (the way it should
    be). I realized why this would be a bad idea a few minutes later, so I'm
    committing now and backing up.
    
    To explain my thinking as to why this is a bad idea: Each suggestion
    really has its own point-of-view that should be considered individually,
    rather than immediately lumping it together with all other suggestions.
    You have to deal with multiple path suggestions for one site (eg. "Darth
    Vader" being suggested as both "Anakin Skywalker" and "Darth Vader") and
    all kinds of other differences that shouldn't be conflated into one
    unvetted entry.

--- index.js
+++ index.js
@@ -8,12 +8,43 @@ var mongoserver = new mongodb.Server('localhost', 27017, {auto_reconnect: true})
 var db_connector = new mongodb.Db('wikilinker', mongoserver, {});
 var db = db_connector.open(function(){})
 var topix = db.collection('topics')
+var suggs = db.collection('suggestions')
 
 var api = express();
 
 api.GET('/topics/:topic',function(req,res){
-  topics.findOne({topic: req.params.topic},function(err,doc){
+  var cursor = topix.find({topic: req.params.topic})
+  cursor.count(function(count,doc){
+    if(count==0){
+      //return a 404
+    }else{
+      //return first document from cursor
+    }
+  })
+})
 
+api.POST('/topics/:topic/suggest',function(req,res){
+  var site, path
+  if(req.params.site && req.params.path) {
+    site = req.params.site
+    path = req.params.path
+  }
+  else {
+    if(req.params.url) {
+
+    } else {
+
+    }
+
+  }
+  var sugcurs = suggs.find({topic: req.params.topic})
+  sugcurs.count(function(err,sugct){
+    //if a suggestion item for this topic name already exists
+    if(sugct>0) {
+      sugct.nextObject(function(err,doc){
+      //if this site already has a suggestion
+      if sites[site]
+    }
   })
 })
 
