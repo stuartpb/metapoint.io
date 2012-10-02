4baa7eb07bc2ccc125920023c9afbf899d8f9f8c
Stuart P. Bentley <stuart@testtrack4.com>
2012-10-02 05:40:18 -0700

    No more WRITEMEs

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var express = require('express');
-var mongo = require('mongodb');
+var mongodb = require('mongodb');
 var https = require('https');
 var http = require('http');
 
-var config = require('config.json');
+var config = require('./config.json');
 
 var nop = function(){}
 
 var mongoserver = new mongodb.Server(config.mongo.server,
   config.mongo.port, {auto_reconnect: true});
-var db_connector = new mongodb.Db('wikilinker', mongoserver, {});
+var db_connector = new mongodb.Db('metapoint', mongoserver, {});
 var db = db_connector.open(nop)
 var topix = db.collection('topics')
 var suggs = db.collection('suggestions')
 
 var api = express();
+
+//Take a response object and respond with the
+//given HTTP code and error message.
+function errespond(res, code, message) {
+  //I flip-flop between res.send versus res.json.
+  res.send(code,{
+    message: message
+    //the HTTP code should indicate it's an error-
+    //JS Error objects also have "name" fields but nah
+  })
+}
+
+//FEATURE: return 405 codes for incorrect verbs
+  //on valid API paths
 
 api.GET('/topics/:topic',function(req,res){
   var cursor = topix.find({topic: req.query.topic})
@@ -30,7 +44,8 @@ api.GET('/topics/:topic',function(req,res){
       //FEATURE: redirects / aliases
 
       //return a 404
-      //WRITEME
+      //this could probably be coerced to be a bit more helpful
+      errespond(res,404,"Not Found")
     } else {
       //FEATURE: Disambiguation (if count > 1)
         //NEEDS: Scope fields (and scope query support)
@@ -51,43 +66,71 @@ api.GET('/topics/:topic',function(req,res){
         //  the songs, the Soulcalibur character).
 
       //return first document from cursor
-      //FEATURE: allow subset requests via query string
+      cursor.nextObject(function(err,doc){
+        //allow pages on any domain to read this data
+        res.setHeader("Access-Control-Allow-Origin",'*')
+
+        //FEATURE: allow requests for a subset of the data
+        //(ie. just the en.wikipedia.org page) via query string
+        res.send(doc)
+      })
+
     }
   })
 })
 
 api.POST('/topics/:topic/suggest',function(req,res){
-  //FEATURE: strip http / https from paths
-  var site, path
-  if(req.query.site && req.query.path) {
-    site = req.query.site
-    path = req.query.path
-  }
-  else {
+  var host = req.query.host
+  var path = req.query.path
+  if(!(host && path)){
     if(req.query.url) {
-      //split the URL and figure it out from there
-      //WRITEME
+      //kudos to Douglas Crockford (I'd cut this up into concatenated strings
+      //for cleanliness and compile it globally but nah)
+      //Note that this has been tweaked so capture group 5 contains
+      //the slash, query, and hash parts (and symbols) of the path
+      //NOTE: this won't match URLs with IPv6 hosts, but if someone is sending one of those
+      //they have a whoooole other class of problem.
+      var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(\/.*)?$/;
+      var result = parse_url.exec(req.query.url)
+
+      //CONSIDER: What to do with the scheme and slashes? It's not out of the
+        //question that http vs. https could matter for the host.
+
+      //this is kind of redundant but not quite?
+      if(result && result[3] && result[5]) {
+        host = result[3]
+        path = result[5]
+      } else {
+        errespond(400,"URL malformed")
+      }
     } else {
       //Respond with an error saying that a URL must be included
       //if site and path are not
-      //WRITEME
+      errespond(400,"No URL or site/path pair")
     }
   }
+  //FEATURE: Strip/use URL prefixes from hosts?
 
-  //FEATURE: verify valid paths
+  //FEATURE: verify valid host/path
+    //Ensure they don't contain invalid characters
+    //Check if the host/page exists?
 
   //Create a new suggestion object
   var suggestion = {
     topic: req.params.topic,
-    site: site,
-    path: path
+    host: host,
+    path: path,
+    notes: req.query.notes || req.body.notes
   //FEATURE: log identity in some way (key, session, IP address)
+    //Currently this is kind of what the "notes" thing is for
   }
 
   //Add suggestion object to suggestions database
-  //WRITEME
+  suggs.insert(suggestion)
 
 }) // /topics/:topic/suggest
+
+//FEATURE: Non-API app, with a human page or two
 
 var app = express();
 app.use('/api/v0',api)
