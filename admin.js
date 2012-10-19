5049d7f56b56d9e2dc81dcc45d278203d296f753
Stuart P. Bentley <stuart@testtrack4.com>
2012-10-19 15:56:02 -0700

    Closer-to-working admin controls

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-//Admin control panel for wikilinker.
+//Admin control panel for metapoint.
 //Note that this, as written, has no authentication.
 
 //USING THINGS THAT MODIFY THE DATABASE SHOULD HAVE AUTHENTICATION.
@@ -8,40 +8,32 @@
 //but obviously, SOME FORM OF AUTHENTICATION SHOULD BE ADDED
 //BEFORE GOING LIVE WITH THIS.
 
-//REM: The moving-suggestions-to-production-type code should probably
-//be kept somewhere like /lib/admin.js, so it can be used by
-//stuff like automated server-side scripts.
-
 var express = require('express');
 var mongo = require('mongodb');
 var https = require('https');
 var http = require('http');
 
-var config = require('config.json');
-
-var nop = function(){}
+var config = require('./config.json');
+var adminlib = require('.lib/admin.js');
 
 var mongoserver = new mongodb.Server(config.mongo.server,
   config.mongo.port, {auto_reconnect: true});
 var db_connector = new mongodb.Db(config.mongo.database, mongoserver, {});
-var db = db_connector.open(nop)
+var db = db_connector.open()
 var topix = db.collection('topics')
 var suggs = db.collection('suggestions')
 //All actions taken in the admin cp should be logged.
-var log = db.collection('log')
+var log = db.collection('oplog')
 
 var admin = express();
 
-//GET /suggestions
-//A page that lists all current suggestions,
-//with editing and a "Merge" (make it official) button.
-//Each suggestion also has a "Drop" checkbox:
-//if checked, the suggestion is deleted when merged
-//(and its UI element disappears).
-//WRITEME
+admin.get('/suggestions',function(req,res){
+  var top20 = suggs.find().limit(20)
+  res.render('admin/suggestions',{suggestions: top20})
+})
 
 //POST /api/merge
-//Works basically like /topics/:topic/suggest
+//Works basically like /suggest
 //in the main API, but with the topic being a query
 //parameter instead of a path parameter.
 //Also takes a "sid" parameter for the
@@ -49,11 +41,15 @@ var admin = express();
 //every invocation of this operation is going to be tied
 //to a suggestion that should then be immediately deleted,
 //and why require two API calls?
-//WRITEME
+admin.post('/api/merge',function(req,res){
+  res.send(501,'Not Implemented')
+})
 
 //POST /api/drop
 //Just the "delete suggestion" part of the above API call.
-//WRITEME
+admin.post('/api/drop',function(req,res){
+  res.send(501,'Not Implemented')
+})
 
 //Removing/editing the existing topics database entries
 //is the kind of thing I'll just leave to the straight Mongo CLI.
