da5c6ed0e4605fad0b86b85aa29305ee1a447a11
Stuart P. Bentley <stuart@testtrack4.com>
2012-11-08 02:55:36 -0800

    Update mongo/welp-integrate.js to support different scopes

--- mongo/welp-integrate.js
+++ mongo/welp-integrate.js
@@ -1,17 +1,33 @@
+//run this script with `--eval "var iScope = /film$/"`
+//to integrate all films
+
+var integrateScope = iScope || null
+
 db = db.getSiblingDB("metapoint")
 
-db.suggestions.find({
+var cursor = db.suggestions.find({
   host: 'en.wikipedia.org', scope: null
-}).forEach(function(doc) {
-  db.topics.update({
-    topic: doc.topic,
-    scope: null
-  },{ $set:{
-    'sites.en_wikipedia_org': doc.path
-  }},true)
-  db.oplog.insert({
-    action: 'transfer',
-    suggestion: doc
-  })
-  db.suggestions.remove({_id: doc._id})
 })
+var count = cursor.count()
+if (count > 0) {
+  var i = 0, lastPct = 0
+  cursor.forEach(function(doc) {
+    db.topics.update({
+      topic: doc.topic,
+      scope: integrateScope
+    },{ $set:{
+      'sites.en_wikipedia_org': doc.path
+    }},true)
+    db.oplog.insert({
+      action: 'transfer',
+      suggestion: doc
+    })
+    db.suggestions.remove({_id: doc._id})
+    ++i;
+    if((count/i)%1>lastPct) {
+      print(i+' of '+count+' processed ('+((count/i)%1)+'%)')
+    }
+  })
+} else {
+  print("No such suggestions found (did you set iScope?)")
+}
