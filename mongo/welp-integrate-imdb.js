98e43ce2fa8a547dc34c42f1522a99d66d2c3a64
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-10 20:45:06 -0500

    Mongo integration scripts split

---
+++ mongo/welp-integrate-imdb.js
@@ -0,0 +1,78 @@
+/*global iScope db*/
+
+//run this script with `--eval "var iScope = /film$/"`
+//to integrate all films
+
+//Don't forget to specify the database on the command line!
+
+var integrateScope = iScope || null;
+
+var integrateHost = 'www.imdb.com';
+
+function parseTemplateParams(captures){
+  var position = 1;
+  var results = {};
+
+  //Skip 0, since that's just the whole match
+  for(var i=1; i < captures.length; i++){
+    var sep = captures[i].indexOf('=');
+
+    if(sep >= 0){
+      results[captures[i].slice(0,sep).trim()] = captures[i].slice(sep+1).trim;
+    } else {
+      results[position] = captures[i].trim();
+      ++position;
+    }
+  }
+
+  return results;
+}
+
+var cursor = db.suggestions.find({
+  host: integrateHost,
+  scope: integrateScope
+});
+var count = cursor.count();
+if (count > 0) {
+  var i = 0, lastPct = 0;
+  cursor.forEach(function(doc) {
+    var params = parseTemplateParams(doc.notes.match(
+      /^Capture: \{\{[^|]*(?:\|([^\|]*))*\}\}$/m));
+
+    var id = params['1'] || params.id;
+    var title = params['2'] || params.title;
+    var description = params['3'] || params.description;
+
+    if(
+      //everything would have needed an ID to get this far-
+      //this is basically just a sanity check
+      id &&
+      //If there's a description, it probably needs future scrutiny
+      !description &&
+      //If there's a title, only approve it if it's the same as the page title
+      (!title || title == doc.title)){
+
+      var updata = {};
+      updata['sites.'+doc.host.replace(/\./g,'_')] = doc.path;
+
+      db.topics.update({
+        topic: doc.topic,
+        scope: doc.scope
+      },{ $set: updata },true);
+      db.oplog.insert({
+        action: 'transfer',
+        suggestion: doc
+      });
+      db.suggestions.remove({_id: doc._id});
+    }
+    ++i;
+    if (Math.floor((count/i)*100) > lastPct) {
+      print( i + ' of ' + count + ' processed '+
+        '(' + Math.floor((count/i)*100) + '%)');
+      lastPct = Math.floor((count/i)*100);
+    }
+  });
+} else {
+  print("No such suggestions found "+
+    "(did you set iScope and specify the database?)");
+}
