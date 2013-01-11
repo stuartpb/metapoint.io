973088df24872589b8af1da213d4f3b1d11f4af4
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-10 20:45:36 -0500

    Add some semicolons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var https = require('https');
 var http = require('http');
 
 //for reading in files for HTTPS setup
-var fs = require('fs')
+var fs = require('fs');
 
 var config = require('./config.json');
 
@@ -15,25 +15,25 @@ var db_connector = new mongodb.Db(config.mongo.database, mongoserver, {});
 db_connector.open(function(err,db) {
   var app = express();
 
-  app.use('/api/v0',require('./subapps/api.js')(db))
+  app.use('/api/v0',require('./subapps/api.js')(db));
   if (config.admin) {
-    app.use(config.admin,require('./subapps/admin/app.js')(db,config.admin))
+    app.use(config.admin,require('./subapps/admin/app.js')(db,config.admin));
   }
 
-  app.use(express.static(__dirname+'/static'))
+  app.use(express.static(__dirname+'/static'));
 
   if (config.http) {
     http.createServer(app).listen(config.http.port);
   }
   if (config.https) {
-    var httpsoptions = config.https.options || {}
+    var httpsoptions = config.https.options || {};
     //The list of configurable filenames.
-    var httpsfiles = ['pfx','key','cert','ca']
+    var httpsfiles = ['pfx','key','cert','ca'];
     //Read in any specified files
     for (var i = 0; i < httpsfiles.length; i++) {
       if (config.https.files[httpsfiles[i]]) {
         httpsoptions[httpsfiles[i]] = fs.readFileSync(
-          config.https.files[httpsfiles[i]])
+          config.https.files[httpsfiles[i]]);
       }
     }
     https.createServer(httpsoptions, app).listen(config.https.port);
--- subapps/api.js
+++ subapps/api.js
@@ -8,25 +8,25 @@ function errespond(res, code, message) {
     message: message
     //the HTTP code should indicate it's an error-
     //JS Error objects also have "name" fields but nah
-  })
+  });
 }
 
 function pagelist(db){
-  var topix = db.collection('topics')
+  var topix = db.collection('topics');
 
   return function(req,res){
     //allow pages on any domain to read this data,
     //even if it's an error or something
-    res.setHeader("Access-Control-Allow-Origin",'*')
+    res.setHeader("Access-Control-Allow-Origin",'*');
 
     var cursor = topix.find({
       topic: req.param('topic'),
       //TODO: scope should be handled better than this
       scope: req.param('scope')
-    })
+    });
     cursor.count(function(err,count){
       if(err){
-        errespond(res,500,err)
+        errespond(res,500,err);
       } else if(count==0){
         //FEATURE: case-insensitivity
           //NEEDS: lc_topic field on topic entries
@@ -64,34 +64,34 @@ function pagelist(db){
           //FEATURE: allow requests for a subset of the data
           //(ie. just the en.wikipedia.org page) via query string
           res.send(doc);
-        }) //cursor.nextObject callback
+        }); //cursor.nextObject callback
       } // if (err) else if (count==0) else
-    }) //cursor.count callback
-  } // /pagelist callback
+    }); //cursor.count callback
+  }; // /pagelist callback
 }// pagelist constructor
 
 function suggest(db){
   //The collection to add to.
-  var suggs = db.collection('suggestions')
+  var suggs = db.collection('suggestions');
 
   //Bounce redundant suggestions (in the future there may be better ways)
   //Need topic connection to do that
-  var topix = db.collection('topics')
+  var topix = db.collection('topics');
 
   return function(req,res){
-    var topic = req.param('topic')
-    var scope = req.param('scope')
+    var topic = req.param('topic');
+    var scope = req.param('scope');
     if(scope===''){scope=null}
     if(req.param('url')) {
-      var urlObj = url.parse(req.param('url'))
+      var urlObj = url.parse(req.param('url'));
 
       //CONSIDER: What to do with the scheme and slashes? It's not out of the
         //question that http vs. https could matter for the host.
 
       //this is kind of redundant but not quite?
       if(urlObj.host && urlObj.path) {
-        var host = urlObj.host
-        var path = urlObj.path
+        var host = urlObj.host;
+        var path = urlObj.path;
 
         //FEATURE: Strip/use URL prefixes from hosts?
     
@@ -106,84 +106,84 @@ function suggest(db){
           scope: scope,
           host: host,
           path: path,
-        }
+        };
         
-        var topicResult, suggestionResult
+        var topicResult, suggestionResult;
         
         var topicQuery = {
           topic: topic,
           scope: scope,
           sites:{}  
-        }
+        };
         
-        topicQuery.sites[host.replace(/\./g,'_')] = path
+        topicQuery.sites[host.replace(/\./g,'_')] = path;
         
         topix.count(topicQuery,function(err,count){
           if(err){
-            errespond(res,500,err)
+            errespond(res,500,err);
           } else if(count > 0){
-            topicResult = 'found'
-            moveForward()
+            topicResult = 'found';
+            moveForward();
           } else {
-            topicResult = 'not found'
-            moveForward()
+            topicResult = 'not found';
+            moveForward();
           }
-        })
+        });
         
         suggs.count(suggestion,function(err,count){
           if(err){
-            errespond(res,500,err)
+            errespond(res,500,err);
           } else if(count > 0){
-            suggestionResult = 'found'
-            moveForward()
+            suggestionResult = 'found';
+            moveForward();
           } else {
-            suggestionResult = 'not found'
-            moveForward()
+            suggestionResult = 'not found';
+            moveForward();
           }
-        })
+        });
         
         var moveForward = function() {
           if(topicResult && suggestionResult) {
             if(topicResult == 'found'){
-              res.send({result:'Host already resolves to path for given topic'})
+              res.send({result:'Host already resolves to path for given topic'});
             } else if(suggestionResult == 'found') {
-              res.send({result:'Suggestion already noted'})
+              res.send({result:'Suggestion already noted'});
             } else {
               //Add remaining suggestion parameters
-              suggestion.notes = req.param('notes')
+              suggestion.notes = req.param('notes');
               //FEATURE: log identity in some way (key, session, IP address)
                 //Currently this is kind of what the "notes" thing is for
          
               //Add suggestion object to suggestions database
-              suggs.insert(suggestion)
+              suggs.insert(suggestion);
           
               //Return a success code
-              res.send({result:"Suggestion added"})
+              res.send({result:"Suggestion added"});
             }
           }
-        } //otherwise we're still waiting for all the results, do nothing
+        }; //otherwise we're still waiting for all the results, do nothing
       } else {
-        errespond(res,400,"URL malformed")
+        errespond(res,400,"URL malformed");
       }
     } else {
       //Respond with an error saying that a URL must be included
       //if site and path are not
-      errespond(res,400,"No URL or site/path pair")
+      errespond(res,400,"No URL or site/path pair");
     }
-  } // /suggest callback
+  }; // /suggest callback
 }// suggest constructor
 
 //App constructor
 module.exports = function(db){
   var api = express();
 
-  api.use(express.bodyParser())
+  api.use(express.bodyParser());
 
   //FEATURE: return 405 codes for incorrect verbs
     //on valid API paths
 
-  api.get('/pagelist',pagelist(db))
-  api.post('/suggest',suggest(db))
+  api.get('/pagelist',pagelist(db));
+  api.post('/suggest',suggest(db));
 
-  return api
-}
+  return api;
+};
