803a6e6cb4a4acab7fc637f912b15e33ba1ae678
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-12 08:31:03 -0500

    Change untransferrer from cautious to hold-onto-your-butts

--- mongo/untransfer-imdb.js
+++ mongo/untransfer-imdb.js
@@ -1,8 +1,14 @@
-/*global db*/
+/*global db ObjectId*/
 
-db.oplog.find({action:'transfer','suggestion.host':'www.imdb.com'})
-  //excercise great caution
-  .sort({_id:-1}).limit(1).forEach(function(doc){
+var cursor = db.oplog.find({action:'transfer','suggestion.host':'www.imdb.com',
+//do not include the op I already untransferred
+ "_id" : {$lt:ObjectId("50ef7889dae964d511fa09fc")}});
+
+  //here goes nothing
+var count = cursor.count();
+if (count > 0) {
+  var i = 0, lastPct = 0;
+  cursor.forEach(function(doc) {
     db.oplog.insert({action:'untransfer',
       unopid:doc._id,suggid:doc.suggestion._id});
     db.suggestions.insert(doc.suggestion);
@@ -10,4 +16,13 @@ db.oplog.find({action:'transfer','suggestion.host':'www.imdb.com'})
     //if I'd clobbered existing data, who knows how I'd have gotten it back?
     db.topics.update({topic:doc.suggestion.topic},
       {$unset:{'sites.www_imdb_com':''}});
-});
+    ++i;
+    if (Math.floor((i/count)*100) > lastPct) {
+      print( i + ' of ' + count + ' processed '+
+        '(' + Math.floor((i/count)*100) + '%)');
+      lastPct = Math.floor((i/count)*100);
+    }
+  });
+} else {
+  print("No such ops found (phew/uh oh?)");
+}
