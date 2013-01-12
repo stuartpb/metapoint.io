f68b0556d07f6296ccac97492470bc3388eae098
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-12 10:18:43 -0800

    Change untransfer-imdb to work for ops that might have already been unopped

--- mongo/untransfer-imdb.js
+++ mongo/untransfer-imdb.js
@@ -1,21 +1,23 @@
 /*global db ObjectId*/
 
-var cursor = db.oplog.find({action:'transfer','suggestion.host':'www.imdb.com',
-//do not include the op I already untransferred
- "_id" : {$lt:ObjectId("50ef7889dae964d511fa09fc")}});
+var skip = skip || 0;
 
-  //here goes nothing
+var cursor = db.oplog.find({action:'transfer',
+  'suggestion.host':'www.imdb.com'}).sort({_id:1});
+
 var count = cursor.count();
 if (count > 0) {
   var i = 0, lastPct = 0;
   cursor.forEach(function(doc) {
-    db.oplog.insert({action:'untransfer',
-      unopid:doc._id,suggid:doc.suggestion._id});
-    db.suggestions.insert(doc.suggestion);
-    //I'd really be more boned if this weren't exactly how it went down -
-    //if I'd clobbered existing data, who knows how I'd have gotten it back?
-    db.topics.update({topic:doc.suggestion.topic},
-      {$unset:{'sites.www_imdb_com':''}});
+    if(!db.oplog.findOne({unopid:doc._id})){
+      db.oplog.insert({action:'untransfer',
+        unopid:doc._id,suggid:doc.suggestion._id});
+      db.suggestions.insert(doc.suggestion);
+      //I'd really be more boned if this weren't exactly how it went down -
+      //if I'd clobbered existing data, who knows how I'd have gotten it back?
+      db.topics.update({topic:doc.suggestion.topic},
+        {$unset:{'sites.www_imdb_com':''}});
+    }
     ++i;
     if (Math.floor((i/count)*100) > lastPct) {
       print( i + ' of ' + count + ' processed '+
