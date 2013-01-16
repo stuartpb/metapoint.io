0d2ddfd12bf935c7f20055cc7c9baf4e9eef3ae7
Stuart P. Bentley <stuart@testtrack4.com>
2013-01-16 04:26:35 -0500

    Tighten API code (semicolons, a comment, remove unnecesarry require)

--- subapps/admin/api.js
+++ subapps/admin/api.js
@@ -1,7 +1,6 @@
 //Metapoint admin console API
 //Basically there for AJAX calls from the views
 var express = require('express');
-var url = require('url');
 var ObjectID = require('mongodb').ObjectID;
 
 //POST /merge
@@ -15,23 +14,23 @@ var ObjectID = require('mongodb').ObjectID;
 //and why require two API calls?
 function merge(db){
   //All actions taken in the admin cp should be logged.
-  var oplog = db.collection('oplog')
-  var topix = db.collection('topics')
-  var suggs = db.collection('suggestions')
+  var oplog = db.collection('oplog');
+  var topix = db.collection('topics');
+  var suggs = db.collection('suggestions');
 
   return function(req,res){
-    var reqtopic = req.param('topic')
-    var reqscope = req.param('scope') || null
-    var reqhost = req.param('host')
-    var reqpath = req.param('path')
-    var reqnotes = req.param('notes')
-    var reqsid = req.param('sid')
+    var reqtopic = req.param('topic');
+    var reqscope = req.param('scope') || null;
+    var reqhost = req.param('host');
+    var reqpath = req.param('path');
+    var reqnotes = req.param('notes');
+    var reqsid = req.param('sid');
 
-    var updata = {}
-    updata['sites.'+reqhost.replace(/\./g,'_')] = reqpath
+    var updata = {};
+    updata['sites.'+reqhost.replace(/\./g,'_')] = reqpath;
 
     if(reqsid){
-      var sidoid = new ObjectID(reqsid)
+      var sidoid = new ObjectID(reqsid);
       oplog.insert({
         action: 'upsert',
         suggestion: {
@@ -42,8 +41,8 @@ function merge(db){
           notes: reqnotes,
           _id: sidoid
         }
-      })
-      suggs.remove({_id: sidoid})
+      });
+      suggs.remove({_id: sidoid});
     } else {
       oplog.insert({
         action: 'write',
@@ -54,91 +53,91 @@ function merge(db){
           path: reqpath,
           notes: reqnotes,
         }
-      })
+      });
     }
 
     topix.update({
       topic: reqtopic,
       scope: reqscope
     },{$set: updata},
-      {upsert:true})
+      {upsert:true});
 
-    res.send(200,'OK')
-  }
+    res.send(200,'OK');
+  };
 }
 
 //POST /drop
 //Just the "delete suggestion" part of the above API call.
 function drop(db){
   //All actions taken in the admin cp should be logged.
-  var oplog = db.collection('oplog')
-  var suggs = db.collection('suggestions')
+  var oplog = db.collection('oplog');
+  var suggs = db.collection('suggestions');
 
   return function(req,res){
-    var reqsid = req.param('sid')
+    var reqsid = req.param('sid');
     if(reqsid) {
-      var sidoid = new ObjectID(reqsid)
+      var sidoid = new ObjectID(reqsid);
       suggs.findOne({_id: sidoid},function(err,doc){
         if(err){
-          res.send(500,err)
+          res.send(500,err);
         } else {
           oplog.insert({
             action: 'forget',
             suggestion: doc
-          })
-          suggs.remove({_id: sidoid})
-          res.send(200)
+          });
+          suggs.remove({_id: sidoid});
+          res.send(200);
         }
-      })
+      });
     } else {
-      var query = {}
+      var query = {};
       if (req.param('topic')) { query.topic = req.param('topic') }
       if (req.param('scope')) { query.scope = req.param('scope') }
       else if(req.param('scope') === ""){
-        query.scope = null
+        query.scope = null;
       }
       if (req.param('host')) { query.host = req.param('host') }
       if (req.param('path')) { query.path = req.param('path') }
 
       if(query.host && (query.topic || query.path)){
-        var cursor = suggs.find(query)
-        var found = false
+        var cursor = suggs.find(query);
+        var found = false;
         cursor.each(function(err,doc) {
           if(err){
-            res.send(500,err)
+            res.send(500,err);
           } else {
             if(doc) { //null is returned after iterating through all docs
-              found = true
+              found = true;
               oplog.insert({
                 action: 'forget-by-value',
                 suggestion: doc
-              })
-              suggs.remove({_id: doc._id})
+              });
+              suggs.remove({_id: doc._id});
             } else if(!found) { //if the first run of the each callback did nothing
-              res.send(400, "Requested value not found")
+              res.send(400, "Requested value not found");
             } else {
-              res.send(200)
+              res.send(200);
             }
           }
-        })
+        });
       } else {
-        res.send(400, "Insufficient data to find suggestion")
+        res.send(400, "Insufficient data to find suggestion");
       }
     }
-  }
+  }; //return function(req,res)
 }
 
 //App constructor
 module.exports = function(db){
   var api = express();
 
-  api.use(express.bodyParser())
+  api.use(express.bodyParser());
 
   //FEATURE: return 405 codes for incorrect verbs
     //on valid API paths
 
-  api.post('/merge',merge(db))
-  api.post('/drop',drop(db))
+  api.post('/merge',merge(db));
+  api.post('/drop',drop(db));
 
-  return api
-}
+  return api;
+};
